23dac5c5cdc654822ab1615514d4e5238f934af4
mt <margarettom6@gmail.com>
2020-07-03 08:29:35 +0300

    Using Axios to consume an api

--- scripts.js
+++ scripts.js
@@ -7,81 +7,37 @@ logo.src = 'logo.png'
 const container = document.createElement('div')
 container.setAttribute('class',' container')
 
-app.appendChild(logo)
-app.appendChild(container)
+app.appendChild(logo);
+app.appendChild(container);
 
-let request = new XMLHttpRequest()
 
-// Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+(async () => {
+    const response = await axios({
+        url: 'https://ghibliapi.herokuapp.com/films',
+        method: 'get'
+    })
+        .then(response => {
+            let data = response.data
+            console.log(data)
 
-request.onload = function () {
-    // Begin accessing JSON data here
-    let data = JSON.parse(this.response)
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach((movie) => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card')
+            data.forEach(movie => {
+                const card = document.createElement('div')
+                card.setAttribute('class', 'card')
 
-            const h1 = document.createElement('h1')
-            h1.textContent = movie.title
+                const h1 = document.createElement('h1')
+                h1.textContent = movie.title
 
-            const p = document.createElement('p')
-            movie.description = movie.description.substring(0, 300) //llimiting to 300 chracters
-            p.textContent = `${movie.description}...` //End with an ellipses
+                const p = document.createElement('p')
+                movie.description = movie.description.substring(0, 300) //llimiting to 300 chracters
+                p.textContent = `${movie.description}...`
 
-            
-            //Append the cards to the container element
-            container.appendChild(card)
-
-            //each card will conatin an h1 and a p
-            card.appendChild(h1)
-            card.appendChild(p)
+                container.appendChild(card)
+                card.appendChild(h1)
+                card.appendChild(p)
+            })
         })
-    } else {
-        console.log('error')
-    }
-}
+        .catch(err => {
+            console.log(err)
+        })
+})()
 
-request.send();
-
-// const app = document.getElementById('root')
-
-// const logo = document.createElement('img')
-// logo.src = 'logo.png'
-
-// const container = document.createElement('div')
-// container.setAttribute('class', 'container')
-
-// app.appendChild(logo)
-// app.appendChild(container)
-
-// var request = new XMLHttpRequest()
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-// request.onload = function () {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response)
-//   if (request.status >= 200 && request.status < 400) {
-//     data.forEach((movie) => {
-//       const card = document.createElement('div')
-//       card.setAttribute('class', 'card')
-
-//       const h1 = document.createElement('h1')
-//       h1.textContent = movie.title
-
-//       const p = document.createElement('p')
-//       movie.description = movie.description.substring(0, 300)
-//       p.textContent = `${movie.description}...`
-
-//       container.appendChild(card)
-//       card.appendChild(h1)
-//       card.appendChild(p)
-//     })
-//   } else {
-//     const errorMessage = document.createElement('marquee')
-//     errorMessage.textContent = `Gah, it's not working!`
-//     app.appendChild(errorMessage)
-//   }
-// }
-
-// request.send()
